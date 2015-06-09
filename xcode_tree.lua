0bdd95e489bc7149ceecbd3a201b65643349ab42
James Touton <jtouton@blizzard.com>
2015-06-09 14:09:02 -0700

    Don't process icons twice.

--- xcode_tree.lua
+++ xcode_tree.lua
@@ -273,12 +273,14 @@
 		table.foreachi(prj.files, function(file)
 			local node = tree.add(files, solution.getrelative(sln, file), { kind = 'group' })
 			node.kind = 'file'
-			local settings = prj._.files[file].xcode_filesettings
-			node.settings = next(settings) and settings
-			local category = xcode6.getBuildCategory(node.name)
-			node.category = category
-			node.action = category == 'Sources' and 'build' or
-				category == 'Resources' and 'copy' or nil
+			if file ~= prj.icon then -- icons handled elsewhere
+				local settings = prj._.files[file].xcode_filesettings
+				node.settings = next(settings) and settings
+				local category = xcode6.getBuildCategory(node.name)
+				node.category = category
+				node.action = category == 'Sources' and 'build' or
+					category == 'Resources' and 'copy' or nil
+			end
 		end)
 		table.foreachi(prj.xcode_resources, function(file)
 			file = solution.getrelative(sln, file)
